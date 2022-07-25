7b9e2be27543079e3873c5656498bc45eb7d2e46
R <dr.coder.380@gmail.com>
2022-07-25 12:19:25 +0300

    1

---
+++ UnitTestJenkens/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestJenkens")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestJenkens")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8515a713-b6f7-4cf2-9ce7-fc854c9933db")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestJenkens/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace UnitTestJenkens
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private readonly string expected = "Hello Jenkens";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                JenkensTest.Program.Main();
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(expected, result);
+            }
+        }
+
+    }
+}
